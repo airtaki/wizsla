551db0d0ed94949199338ab85f0287956b0dd2d4
Gabor Takacs <gabor.takacs@frw.co.uk>
2024-04-23 11:09:08 +0200

    Better error handling

--- src/errors/error-handler.ts
+++ src/errors/error-handler.ts
@@ -2,13 +2,30 @@ import util from "util";
 import type { Request, Response, NextFunction } from "express";
 
 export default (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(util.inspect(err, true, null, true));
+  const statusCode = "statusCode" in err ? err.statusCode as number : 500;
+  const errorObject: any = "errorObject" in err ? err.errorObject : {};
+  const cookies = req.get("cookie");
+  console.error(util.inspect({
+    at: new Date(),
+    error: err.name,
+    message: err.message,
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    ...(cookies ? { cookies } : {}),
+    ...(Object.keys(req.body).length > 0 ? { body: req.body } : {}),
+    ...(Object.keys(req.params).length > 0 ? { params: req.params } : {}),
+    ...(Object.keys(req.query).length > 0 ? { query: req.query } : {}),
+    ...(Object.keys(errorObject).length > 0 ? { errorObject } : {}),
+    // Show stack in the log only for internal server errors:
+    ...(statusCode >= 500 && statusCode < 600 ? { stack: err.stack } : {})
+  }, true, null, true));
   res
-    .status("statusCode" in err ? err.statusCode as number : 500)
+    .status(statusCode)
     .setHeader("Content-Type", "application/json")
     .json({
       success: false,
       error: err.message,
-      ...("errorObject" in err ? { details: err.errorObject } : {}),
+      ...(Object.keys(errorObject).length > 0 ? { errorObject } : {})
     });
 };
